bec15e9a7362de13838b3071d98727c2098e8714
MrMojoRising777 <alexander.goyens@gmail.com>
2024-01-16 22:43:35 +0100

    fix migration

--- database/migrations/2024_01_16_213911_create_series_table.php
+++ database/migrations/2024_01_16_213911_create_series_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('series', function (Blueprint $table) {
             $table->id();
-            $table->name();
-            $table->abbreviation();
+            $table->string('name');
+            $table->string('abbreviation');
             $table->timestamps();
         });
     }
