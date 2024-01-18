bfbdd60ef094386cd9e4006d665b9cb76e094ead
MrMojoRising777 <alexander.goyens@gmail.com>
2024-01-18 20:54:34 +0100

    Add several columns to DB

--- database/migrations/2024_01_18_190900_add_volume_to_albums.php
+++ database/migrations/2024_01_18_190900_add_volume_to_albums.php
@@ -11,8 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('username');
+        Schema::table('albums', function (Blueprint $table) {
+            $table->integer('volume')->after('serie_id');
         });
     }
 
@@ -21,8 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('username');
+        Schema::table('albums', function (Blueprint $table) {
+            $table->dropColumn('volume');
         });
     }
 };
